50374df9564c4f3d9c53d06b1c6a085def86be4e
Rodrigo Lucas <rodrigo.mrpascal@gmail.com>
2021-05-19 21:19:09 -0300

    Criar Home e adicionar primeira barra

---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import './style.css';
+
+function Home() {
+    return (
+        <div>
+            <Header />
+            <main className="main">
+                main
+            </main>
+        </div>
+    );
+}
+
+export default Home;
