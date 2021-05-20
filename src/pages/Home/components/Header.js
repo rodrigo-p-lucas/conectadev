2526495cd26780c988a65fa36ba55114ec2c6632
Rodrigo Lucas <rodrigo.mrpascal@gmail.com>
2021-05-19 22:37:19 -0300

    Organizar Toolbar da Home

--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -1,19 +1,49 @@
 import React from 'react';
+import { Button, AppBar, Toolbar, SvgIcon, Avatar } from '@material-ui/core'
+import {makeStyles} from '@material-ui/styles'
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+
+const useStyles = makeStyles({
+    appBar:{
+        boxShadow: 'none',
+    },
+    img:{
+        maxHeight: 55,
+    },
+    grow:{
+        flexGrow: 1,
+    },
+    userSection:{
+        display: 'flex',
+        alignItems: 'center',
+    },
+    button:{
+        marginRight: 10,
+    },
+    bell:{
+        marginRight: 10,
+    }
+})
 
 function Header() {
+    const classes = useStyles();
+
     return (
-        <header className="header">
-            <div className="toolbar">
-                <div className="">
-                    <span>Conecta Dev</span>
+        <AppBar position="fixed" color="inherit" className={classes.appBar}>
+            <Toolbar>
+                <img src="/images/logo.png" alt="logo" className={classes.img} />
+                <div className={classes.grow}></div>
+                <div className={classes.userSection}>
+                    <Button variant="contained" color="primary" className={classes.button}>
+                        Novo Post
+                    </Button>
+                    <SvgIcon className={classes.bell}>
+                        <NotificationsNoneIcon />
+                    </SvgIcon>
+                    <Avatar alt="Remy Sharp" src="/" />
                 </div>
-                <div className="">
-                    <button>Novo Post</button>
-                    <span>img1</span>
-                    <span>img2</span>
-                </div>
-            </div>
-        </header>
+            </Toolbar>
+        </AppBar>
     )
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
+
 import Header from './components/Header';
-import './style.css';
+import NavBar from './components/NavBar';
+import Feed from './components/Feed'
+
+const useStyles = makeStyles({
+    root:{
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    main:{
+        height: '100vh',
+        display: 'flex',
+        width: '1200px',
+        margin: '0 auto',
+    }
+})
 
 function Home() {
+    const classes = useStyles();
+
     return (
-        <div>
+        <div className={classes.root}>
             <Header />
-            <main className="main">
-                main
+            <div className="toolbar"></div>
+            <main className={classes.main}>
+                <NavBar />
+                <Feed />
             </main>
         </div>
     );
